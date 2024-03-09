1a9b2fb4ef51d7fbb5a500eb27ddebc466f56a4e
Marta <marta@MacBook-Pro-de-Marta-6.local>
2024-03-09 20:24:11 +0100

    .mutate() method added and functioning

--- mystery-organism-starter/main.js
+++ mystery-organism-starter/main.js
@@ -13,10 +13,36 @@ const mockUpStrand = () => {
   return newStrand;
 };
 
+let standDna = mockUpStrand();
+console.log(`My stand DNA: ${standDna}`);
 
+//factory function
+const pAequorFactory = (num, arr) => {
+  return {
+    specimenNum: num,
+    dna: arr,
+    mutate(){
+      let randomNum = Math.floor(Math.random() * 15);
+      console.log(`The random base is ${this.dna[randomNum]}`);
+      let returnedRandomBase = returnRandBase();
+      console.log(`The random base returned is ${returnedRandomBase}`);
 
+      if (this.dna[randomNum] !== returnedRandomBase) {
+        this.dna[randomNum] = returnedRandomBase;
+      } else if (this.dna[randomNum] === returnedRandomBase){
+        do{
+          returnedRandomBase = returnRandBase();
+        }while(this.dna[randomNum] === returnedRandomBase);
+        this.dna[randomNum] = returnedRandomBase;
+      }
 
+      console.log(`The new DNA base is ${this.dna[randomNum]}`);
+      return this.dna;
+    }
+  }
+}
 
-
-
-
+let myDnaObj = pAequorFactory(3, standDna);
+console.log(`My DNA object:\n\n{specimenNum:${myDnaObj.specimenNum},\nDNA: ${myDnaObj.dna}}\n`);
+myDnaObj.mutate();
+console.log(`Mutated DNA object:\n\n{specimenNum:${myDnaObj.specimenNum},\nMutated DNA: ${myDnaObj.dna}}\n`);
