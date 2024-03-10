8b1677fd9c25a25a76abb29841cda37379c05621
Marta <marta@MacBook-Pro-de-Marta-6.local>
2024-03-10 10:49:36 +0100

    Mystery Organism .compareDna() method added and finished

--- mystery-organism-starter/main.js
+++ mystery-organism-starter/main.js
@@ -13,20 +13,26 @@ const mockUpStrand = () => {
   return newStrand;
 };
 
-let standDna = mockUpStrand();
-console.log(`My stand DNA: ${standDna}`);
-
 //factory function
 const pAequorFactory = (num, arr) => {
   return {
     specimenNum: num,
     dna: arr,
     mutate(){
+      //Save in a variable the random number that's going to target the base in the array
       let randomNum = Math.floor(Math.random() * 15);
       console.log(`The random base is ${this.dna[randomNum]}`);
+      //save in a variable the random base that will replace our current base located in the random number's position
       let returnedRandomBase = returnRandBase();
       console.log(`The random base returned is ${returnedRandomBase}`);
 
+      /*
+      Control flow evaluating two conditions:
+      
+      1) If the base dna in the random number's position is different from the returnedRandomBase value, this is replaced
+      
+      2) If they're the same. returnedRandomBase will be redeclared each time with a new random base until they're not the same. And then, our base dna in the array it's replaced
+      */
       if (this.dna[randomNum] !== returnedRandomBase) {
         this.dna[randomNum] = returnedRandomBase;
       } else if (this.dna[randomNum] === returnedRandomBase){
@@ -38,11 +44,30 @@ const pAequorFactory = (num, arr) => {
 
       console.log(`The new DNA base is ${this.dna[randomNum]}`);
       return this.dna;
+    },
+    compareDNA(arrDna){
+      //Save in a variable the 'counter' sort to say. I saved in a fraction form because it's more intuitive for me at least
+      let percentageFractionDna = 0/15;
+      
+      //index variable that will iterate over both arrays
+      let i = 0;
+      do{
+        //if the element on both arrays are the same we add one to our percentageFractionDna variable. If they're not the same we print the message on the console to inform
+        if(this.dna[i] === arrDna[i]){
+          percentageFractionDna += 1/15;
+        } else {
+          console.log(`Control flow passes on iteration number ${i}`)
+        }
+        i++;
+      } while(i < this.dna.length && i <= arrDna.length);
+
+      console.log(`\n\n\nSpecimen #1 and specimen #2 have ${percentageFractionDna*100}% in common!\n\n\n`);
     }
   }
 }
 
-let myDnaObj = pAequorFactory(3, standDna);
-console.log(`My DNA object:\n\n{specimenNum:${myDnaObj.specimenNum},\nDNA: ${myDnaObj.dna}}\n`);
-myDnaObj.mutate();
-console.log(`Mutated DNA object:\n\n{specimenNum:${myDnaObj.specimenNum},\nMutated DNA: ${myDnaObj.dna}}\n`);
+let pAequor1 = pAequorFactory(1, mockUpStrand());
+let pAequor2 = pAequorFactory(2, mockUpStrand());
+console.log(`\nBoth pAequors:\n\npAequor1: ${pAequor1.dna}\n\npAequor2: ${pAequor2.dna}\n`)
+
+pAequor1.compareDNA(pAequor2.dna);
