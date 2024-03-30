716bfa8bd955c28351372fb341af34b0f80e52da
Marta <marta@MacBook-Pro-de-Marta-6.local>
2024-03-30 13:55:20 +0100

    Add Print() method but still have to establish more

--- find-your-hat-starting/main.js
+++ find-your-hat-starting/main.js
@@ -1,8 +1,60 @@
-const prompt = require('prompt-sync')({sigint: true});
+const prompt = require("prompt-sync")({ sigint: true });
 
-const hat = '^';
-const hole = 'O';
-const fieldCharacter = '░';
-const pathCharacter = '*';
+const hat = "^";
+const hole = "O";
+const fieldCharacter = "░";
+const pathCharacter = "*";
 
-console.log('Hello')
+class Field {
+	constructor(arr) {
+		this.arr = arr;
+		this.horizontalPosition = 0;
+		this.verticalPosition = 0;
+	}
+
+	print() {
+		let formattedArr = "";
+		for (let i = 0; i < this.arr.length; i++) {
+			formattedArr = formattedArr + `${this.arr[i].join("")}` + "\n";
+		}
+		console.log(formattedArr);
+
+		// Input prompt
+
+		const way = prompt("Which way?");
+
+        way.toLowerCase();
+
+        switch (way) {
+            case 'r':
+                this.horizontalPosition += 1;
+                break;
+            
+            case 'l':
+                this.horizontalPosition -= 1;
+                break;
+            
+            case 'u':
+                this.verticalPosition -= 1;
+                break;
+            
+            case 'd':
+                this.verticalPosition += 1;
+                break;
+        }
+	}
+
+    positionResult(){
+        
+    }
+}
+
+// Tests Instances
+
+const newField = new Field([
+	[pathCharacter, fieldCharacter, hole],
+	[fieldCharacter, hole, hole],
+	[fieldCharacter, fieldCharacter, hat],
+]);
+
+newField.print();
