6faca8b3d693dc67ebda4d9274c30d087e448d15
Ricardo Guzman <ricardoguzman444@gmail.com>
2024-04-16 20:23:22 +0200

    Header banner ready. Search Bar is there but has to be well styled first

--- jamming/src/App.js
+++ jamming/src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { Banner } from './components/banner/banner';
+import { SearchBar } from './components/searchBar/SearchBar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Banner />
       </header>
+      <main className="App-main">
+        <SearchBar />
+      </main>
     </div>
   );
 }
