dd431d3e4277134854aff18041bcdd490396e6d3
Marta <marta@MacBook-Pro-de-Marta-6.local>
2024-04-19 21:43:33 +0200

    Left and right column styled. Still stateless

--- jammming/src/components/SearchTables/TrackColumns/RightColumn/RightColumn.js
+++ jammming/src/components/SearchTables/TrackColumns/RightColumn/RightColumn.js
@@ -1,7 +1,10 @@
-function RightColumn({}) {
-    return (
-        <div>
+import styles from "./RightColumn.module.css";
 
+function RightColumn() {
+    return (
+        <div className={styles.rightColumnContainer}>
+            <input type="text" placeholder="Playlist Title" className={styles.playlistNameInput}/>
+            <button type="submit" className={styles.savePlaylistButton}>Save Playlist</button>
         </div>
     )
 }
