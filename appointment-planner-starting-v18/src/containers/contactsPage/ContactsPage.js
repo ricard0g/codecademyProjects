d452fc73d46493498c6280ed94a4a85c82973291
Marta <marta@MacBook-Pro-de-Marta-6.local>
2024-04-25 21:48:25 +0200

    4th step not finished yet.

--- appointment-planner-starting-v18/src/containers/contactsPage/ContactsPage.js
+++ appointment-planner-starting-v18/src/containers/contactsPage/ContactsPage.js
@@ -2,12 +2,17 @@ import React, { useState, useEffect } from "react";
 
 import { ContactForm } from "../../components/contactForm/ContactForm";
 import { TileList } from "../../components/tileList/TileList";
+import { UNSAFE_DataRouterStateContext } from "react-router-dom";
 
-export const ContactsPage = () => {
+export const ContactsPage = ({contacts, addNewContact}) => {
   /*
   Define state variables for 
   contact info and duplicate check
   */
+  const [name, setName] = useState(null);
+  const [phone, setPhone] = useState(null);
+  const [email, setEmail ] = useState(null);
+  const [duplicate, setDuplicate] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -15,12 +20,25 @@ export const ContactsPage = () => {
     Add contact info and clear data
     if the contact name is not a duplicate
     */
+   if(!duplicate){
+    addNewContact(name, phone, email);
+    setName(null);
+    setPhone(null);
+    setEmail(null);
+   }
   };
 
   /*
   Using hooks, check for contact name in the 
   contacts array variable in props
   */
+  useEffect(() => {
+    for(const contact in contacts){
+      if(contact["name"] === name){
+        setDuplicate(true);
+      }
+    }
+  }, [name])
 
   return (
     <div>
