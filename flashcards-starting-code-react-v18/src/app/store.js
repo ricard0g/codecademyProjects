97a4af147479d0d4ad60c76567629fe33f046edc
Ricardo Guzman Duran <ricardoguzman444@gmail.com>
2024-05-03 13:27:04 +0200

    All steps finished

--- flashcards-starting-code-react-v18/src/app/store.js
+++ flashcards-starting-code-react-v18/src/app/store.js
@@ -2,10 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 // import reducers
 import topicsSliceReducer from "../features/topics/topicsSlice";
 import quizzesSliceReducer from "../features/quizzes/quizzesSlice";
+import cardsReducer from "../features/cards/cardsSlice"
 
 export default configureStore({
   reducer: {
     topics: topicsSliceReducer,
     quizzes: quizzesSliceReducer,
+    cards: cardsReducer,
   },
 });
--- flashcards-starting-code-react-v18/src/components/NewQuizForm.js
+++ flashcards-starting-code-react-v18/src/components/NewQuizForm.js
@@ -7,6 +7,7 @@ import ROUTES from "../app/routes";
 import { selectTopics } from "../features/topics/topicsSlice";
 import { addTopic } from "../features/topics/topicsSlice";
 import { addQuiz } from "../features/quizzes/quizzesSlice";
+import { addCard } from "../features/cards/cardsSlice";
 
 export default function NewQuizForm() {
   const [name, setName] = useState("");
@@ -25,6 +26,14 @@ export default function NewQuizForm() {
     const cardIds = [];
 
     // create the new cards here and add each card's id to cardIds
+    cards.forEach(() => {
+      const cardId = uuidv4();
+      cardIds.push(cardId);
+      dispatch(addCard({
+        ...cards,
+        id: cardId
+      }));
+    });
     // create the new quiz here
 
     const quizId = uuidv4();
